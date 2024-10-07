f93e296713632e0dd5a29d892658b09eedc4ef20
Efuwa Akyere <efuwayorke13@gmail.com>
2024-10-07 11:07:02 +0000

    Start of Project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+
+export default Navbar;
