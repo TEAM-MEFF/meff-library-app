5bba1336cf3b603772b85b4f3fa034657ad4a45a
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2024-10-09 03:36:37 +0000

    Wednesday dawn on Hero page

--- src/components/NavbarResponsive.jsx
+++ src/components/NavbarResponsive.jsx
@@ -4,44 +4,43 @@ import IonIcon from '@reacticons/ionicons';
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
+import SearchBox from './SearchBox';
+
 
 const NavbarResponsive = () => {
 
     let [open, setOpen] = useState(false);
 
     return (
-        <header className='flex justify-center md:flex-col bg-slate-100'>
-            <div className='flex items-center md:w-[92%]  mx-auto justify-between py-2 bg-slate-200'>
-                <div className="content-center items-center md:mr-0">
+        <header className='flex justify-between md:flex-col bg-slate-200 fixed w-full'>
+            <div className='flex items-center md:w-[92%]  mx-auto justify-center md:justify-between py-2 bg-slate-200'>
+                <div className="content-center items-center">
                     <Link href="#">
-                        <span className="bg-[#0E345A] px-4 py-2  text-white font-bold content-center text-center">MEFF</span>
-                        <span href="#" className="px-2 py-2 content-center hover:bg-[#0E345A] hover:text-white border-2 text-center">LIBRARY</span></Link>
+                        <span className="bg-[#0E345A] text-lg px-4 py-2  text-white font-bold content-center text-center">MEFF</span>
+                        <span href="#" className=" px-2 py-1.5 text-lg text-[#0E345A] content-center hover:bg-[#0E345A] hover:text-white border-[#0E345A] border- border-2 text-center">LIBRARY</span></Link>
                 </div>
-                <div className='flex justify-center relative'>
-                    <input type="search" name="search" id="search" className='' />
-                    <Link><IonIcon name="search" className="absolute right-2 top-1" /></Link>
-                </div>
+                <SearchBox />
                 <div className='flex items-center gap-2'>
-                    <Link><IonIcon name="person-circle" className="text-3xl" /></Link>
-                    <Link><IonIcon name="cart-outline" className="text-3xl" /></Link>
+                    <Link><IonIcon name="person-circle" className="text-4xl md:text-5xl text-[#0E345A]" /></Link>
+                    <Link><IonIcon name="cart-outline" className="text-4xl md:text-5xl text-[#0E345A]" /></Link>
                 </div>
             </div >
-            <nav className='flex items-center gap-2 md:w-[92%]  mx-auto justify-center  py-2 bg-slate-200'>
-                <div className={`sm:static sm:min-h-fit sm:w-auto md:min-h-fit md:w-auto md:z-auto z-[1] px-12 absolute bg-slate-300 min-h-[40vh] left-0 w-full flex items-center transition-all duration-500 ease-in-out ${open ? 'top-10 opacity-100' : 'top-[-500px] md:opacity-100 opacity-0'}`}>
-                    <ul className='flex sm:items-center sm:gap-1 sm:flex-row md:items-center gap-2 md:flex-row flex-col transition-all duration-1000 ease-in-out'>
-                        <li><a href="#" className='sm:text-base md:text-lg px-2 py-1 hover:bg-slate-500 hover:text-white rounded'>Home</a></li>
-                        <li><a href="#" className='sm:text-base md:text-lg px-2 py-1 hover:bg-slate-500 hover:text-white rounded'>Bookstore</a></li>
-                        <li><a href="#" className='sm:text-base md:text-lg px-2 py-1 hover:bg-slate-500 hover:text-white rounded'>Best Selling</a></li>
-                        <li><a href="#" className='sm:text-base md:text-lg px-2 py-1 hover:bg-slate-500 hover:text-white rounded'>Events</a></li>
-                        <li><a href="#" className='sm:text-base md:text-lg px-2 py-1 hover:bg-slate-500 hover:text-white rounded'>About Us</a></li>
-                        <li><a href="#" className='sm:text-lg md:text-lg px-2 py-1 hover:bg-slate-500 hover:text-white rounded'>Contact Us</a></li>
-                    </ul>
-                </div>
-                <div onClick={() => setOpen(!open)} className='menuIcon text-[#0E345A] mx-2 cursor-pointer md:hidden text-2xl'>
+            <nav className='flex items-center gap-2 md:w-[92%]  mx-auto py-2 justify-center bg-slate-200'>
+                <div className={`sm:min-h-fit  md:min-h-fit md:w-auto md:z-auto z-[1] px-12 py-6 md:py-0 absolute md:static bg-slate-200 min-h-[40vh] right-0  flex items-center transition-all duration-500 ease-in-out ${open ? 'top-14 opacity-100' : 'top-[-500px] md:opacity-100 opacity-0'}`}>
+                    <div className='flex sm:gap-5 sm:flex-row md:items-center gap-2 md:flex-row flex-col transition-all duration-1000 ease-in-out'>
+                        <Link className='sm:text-base md:text-lg px-2 py-1 hover:bg-[#0E345A] hover:text-white rounded'>Home</Link>
+                        <Link className='sm:text-base md:text-lg px-2 py-1 hover:bg-[#0E345A] hover:text-white rounded'>Bookstore</Link>
+                        <Link className='sm:text-base md:text-lg px-2 py-1 hover:bg-[#0E345A] hover:text-white rounded'>Best Selling</Link>
+                        <Link className='sm:text-base md:text-lg px-2 py-1 hover:bg-[#0E345A] hover:text-white rounded'>Events</Link>
+                        <Link className='sm:text-base md:text-lg px-2 py-1 hover:bg-[#0E345A] hover:text-white rounded'>About Us</Link>
+                        <Link className='sm:text-base md:text-lg px-2 py-1 hover:bg-[#0E345A] hover:text-white rounded'>Contact Us</Link>
+                    </div>
+                </div >
+                <div onClick={() => setOpen(!open)} className='menuIcon text-[#0E345A] mx-2 cursor-pointer md:hidden text-4xl'>
                     <IonIcon name={open ? 'close' : 'menu'} />
                     {/* <IonIcon name="close" /> */}
                 </div>
-            </nav>
+            </nav >
         </header >
 
     )
--- src/pages/booklist/index.jsx
+++ src/pages/booklist/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Hero from "./components/Hero";
+import Hero from "../../components/Hero";
 import Testimonials from "./components/Testimonials";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
@@ -8,12 +8,12 @@ import NavbarResponsive from "../../components/NavbarResponsive";
 
 const BookList = () => {
   return (
-    <div>
-      {/* <Navbar /> */}
+    <div className="landingPage">
       <NavbarResponsive />
+      <Hero />
       <Link to="/add-new">Add Book</Link>
       <div>BookList Content goes here</div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
