1741ea0c912efed8ff5d30b10dc0725af2e7bc42
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2024-10-10 15:12:35 +0000

    book fetch with dummy text

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,71 @@
-export const BASE_URL="https://meff-library-api.onrender.com";
+export const BASE_URL = "https://meff-library-api.onrender.com";
+
+const K = {
+    BOOKS: [
+        {
+            img: 'src/assets/images/bookImages/captainAmerica.jpeg',
+            id: 1,
+            title: "Captain America",
+            author: "Yuval Noah Harari",
+            genre: "History"
+        },
+        {
+            img: 'src/assets/images/bookImages/gotg.jpg',
+            id: 2,
+            title: "1984",
+            author: "George Orwell",
+            genre: "Dystopian"
+        },
+        {
+            img: 'src/assets/images/bookImages/zootopia.jpeg',
+            id: 3,
+            title: "Atomic Habits",
+            author: "James Clear",
+            genre: "Self-help"
+        },
+        {
+            img: 'src/assets/images/bookImages/captainAmerica.jpeg',
+            id: 1,
+            title: "Captain America",
+            author: "Yuval Noah Harari",
+            genre: "History"
+        },
+        {
+            img: 'src/assets/images/bookImages/gotg.jpg',
+            id: 2,
+            title: "1984",
+            author: "George Orwell",
+            genre: "Dystopian"
+        },
+        {
+            img: 'src/assets/images/bookImages/zootopia.jpeg',
+            id: 3,
+            title: "Atomic Habits",
+            author: "James Clear",
+            genre: "Self-help"
+        },
+        {
+            img: 'src/assets/images/bookImages/captainAmerica.jpeg',
+            id: 1,
+            title: "Captain America",
+            author: "Yuval Noah Harari",
+            genre: "History"
+        },
+        {
+            img: 'src/assets/images/bookImages/gotg.jpg',
+            id: 2,
+            title: "1984",
+            author: "George Orwell",
+            genre: "Dystopian"
+        },
+        {
+            img: 'src/assets/images/bookImages/zootopia.jpeg',
+            id: 3,
+            title: "Atomic Habits",
+            author: "James Clear",
+            genre: "Self-help"
+        },
+    ]
+};
+
+export default K;
