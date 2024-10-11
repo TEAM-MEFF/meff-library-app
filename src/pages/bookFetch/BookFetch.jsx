2b4d8115857eb51bd5779ad5448463b241714aef
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2024-10-11 09:43:24 +0000

    Friday morning

--- src/pages/bookFetch/BookFetch.jsx
+++ src/pages/bookFetch/BookFetch.jsx
@@ -32,7 +32,7 @@ const BookFetch = () => {
                                         <div key={index} className="border p-4 rounded-lg shadow-md">
                                             <img src={books.img} alt="book-image" />
                                             <h2 className="text-xl font-semibold">{books.title}</h2>
-                                            <p className="text-gray-600">{books.author.name}</p>
+                                            <p className="text-gray-600">{books.authors}</p>
                                             <p className="text-gray-500">{books.genre}</p>
                                             <div className='flex gap-2'>
                                                 <button className="mt-4 px-4 py-2 border-blue-900 text-blue-900 rounded border-2">
