14583f531272330d5958585d16c94aacbff2913c
Remedy Online <agyemangmichael555@gmail.com>
2024-12-16 21:18:09 +0000

    fixing default-book images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,99 +1,107 @@
-
 export const BASE_URL = "https://meff-library-api.onrender.com";
 
 const K = {
-    BOOKS: [
-        {
-            _id: 'book1',
-            coverPicture: 'src/assets/images/bookImages/captainAmerica.jpeg',
-            title: "Captain America",
-            author: "Yuval Noah Harari",
-            genre: "Action",
-            datePublished: "9/22/2020",
-            content: "This is a nice book!",
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta ."
-        },
-        {
-            _id: 'book2',
-            coverPicture: 'src/assets/images/bookImages/gotg.jpg',
-            title: "Guardians of the Galaxy",
-            author: "George Orwell",
-            genre: "Action",
-            datePublished: "11/17/2024",
-            content: "This is a nice book!",
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta ."
-        },
-        {
-            _id: 'book3',
-            coverPicture: 'src/assets/images/bookImages/zootopia.jpeg',
-            title: "Zootopia",
-            author: "James Clear",
-            genre: "Cartoon",
-            datePublished: "02/11/2024",
-            content: "This is a nice book!",
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta ."
-        },
-        {
-            _id: 'book4',
-            coverPicture: 'src/assets/images/bookImages/robinHood.jpeg',
-            title: "Robin Hood",
-            author: "Van Persie",
-            genre: "Action",
-            datePublished: "8/25/2013",
-            content: "This is a nice book!",
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta ."
-        },
-        {
-            _id: 'book5',
-            coverPicture: 'src/assets/images/bookImages/walkingDead.jpeg',
-            title: "The Walking Dead",
-            author: "George Orwell",
-            genre: "Action",
-            datePublished: "09/01/2019",
-            content: "This is a nice book!",
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta ."
-        },
-        {
-            _id: 'book6',
-            coverPicture: 'src/assets/images/bookImages/xMen.jpg',
-            title: "Xmen - Apocalypse",
-            author: "James Clear",
-            genre: "Fantasy",
-            datePublished: "12/06/2023",
-            content: "This is a nice book!",
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta ."
-        },
-        {
-            _id: 'book7',
-            coverPicture: 'src/assets/images/bookImages/bigRescues.jpeg',
-            title: "Big Rescues",
-            author: "Yuval Noah Harari",
-            genre: "Cartoon",
-            datePublished: "04/08/2022",
-            content: "This is a nice book!",
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta ."
-        },
-        {
-            _id: 'book8',
-            coverPicture: 'src/assets/images/bookImages/gotg.jpg',
-            title: "1984",
-            author: "George Orwell",
-            genre: "Dystopian",
-            datePublished: "29/10/2015",
-            content: "This is a nice book!",
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta ."
-        },
-        {
-            _id: 'book9',
-            coverPicture: 'src/assets/images/bookImages/zootopia.jpeg',
-            title: "Atomic Habits",
-            author: "James Clear",
-            genre: "Self-help",
-            datePublished: "07/01/2023",
-            content: "This is a nice book!",
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta ."
-        },
-    ]
+  BOOKS: [
+    {
+      _id: "book1",
+      coverPicture: "bookImages/captainAmerica.jpeg",
+      title: "Captain America",
+      author: "Yuval Noah Harari",
+      genre: "Action",
+      datePublished: "9/22/2020",
+      content: "This is a nice book!",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta .",
+    },
+    {
+      _id: "book2",
+      coverPicture: "src/assets/images/bookImages/gotg.jpg",
+      title: "Guardians of the Galaxy",
+      author: "George Orwell",
+      genre: "Action",
+      datePublished: "11/17/2024",
+      content: "This is a nice book!",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta .",
+    },
+    {
+      _id: "book3",
+      coverPicture: "src/assets/images/bookImages/zootopia.jpeg",
+      title: "Zootopia",
+      author: "James Clear",
+      genre: "Cartoon",
+      datePublished: "02/11/2024",
+      content: "This is a nice book!",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta .",
+    },
+    {
+      _id: "book4",
+      coverPicture: "src/assets/images/bookImages/robinHood.jpeg",
+      title: "Robin Hood",
+      author: "Van Persie",
+      genre: "Action",
+      datePublished: "8/25/2013",
+      content: "This is a nice book!",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta .",
+    },
+    {
+      _id: "book5",
+      coverPicture: "src/assets/images/bookImages/walkingDead.jpeg",
+      title: "The Walking Dead",
+      author: "George Orwell",
+      genre: "Action",
+      datePublished: "09/01/2019",
+      content: "This is a nice book!",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta .",
+    },
+    {
+      _id: "book6",
+      coverPicture: "src/assets/images/bookImages/xMen.jpg",
+      title: "Xmen - Apocalypse",
+      author: "James Clear",
+      genre: "Fantasy",
+      datePublished: "12/06/2023",
+      content: "This is a nice book!",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta .",
+    },
+    {
+      _id: "book7",
+      coverPicture: "src/assets/images/bookImages/bigRescues.jpeg",
+      title: "Big Rescues",
+      author: "Yuval Noah Harari",
+      genre: "Cartoon",
+      datePublished: "04/08/2022",
+      content: "This is a nice book!",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta .",
+    },
+    {
+      _id: "book8",
+      coverPicture: "src/assets/images/bookImages/gotg.jpg",
+      title: "1984",
+      author: "George Orwell",
+      genre: "Dystopian",
+      datePublished: "29/10/2015",
+      content: "This is a nice book!",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta .",
+    },
+    {
+      _id: "book9",
+      coverPicture: "src/assets/images/bookImages/zootopia.jpeg",
+      title: "Atomic Habits",
+      author: "James Clear",
+      genre: "Self-help",
+      datePublished: "07/01/2023",
+      content: "This is a nice book!",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum in praesentium delectus animi aliquid maxime, assumenda nam dicta .",
+    },
+  ],
 };
 
 export default K;
