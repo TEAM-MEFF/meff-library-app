de9248e142946197055a793899f75bcce74d385a
Remedy Online <agyemangmichael555@gmail.com>
2024-12-17 12:27:29 +0000

    Login page - fully responsive!

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,16 +4,16 @@ import IonIcon from "@reacticons/ionicons";
 
 const SignUp = () => {
   return (
-    <section className="flex flex-col md:flex-row gap-5 px-5 md:px-12 justify-center bg-slate-100 py-12 h-screen">
-      <div className=" flex flex-col items-left justify-center md:w-1/2 gap-4 md:gap-10">
-        <div className="content-center items-center ">
+    <section className="flex flex-col md:flex-row gap-12 px-5 md:px-12 justify-center bg-slate-100 py-12 h-screen">
+      <div className="flex flex-col items-left justify-center md:w-1/2 gap-4 md:gap-10">
+        <div className="content-center items-center">
           <Link to={"/"}>
-            <span className="bg-themeColor text-lg sm:text-xl md:text-2xl lg:text-3xl px-4 py-2  text-white font-bold content-center text-center">
+            <span className="bg-themeColor text-xl md:text-2xl lg:text-3xl md:py-2 sm:px-2 py-1 px-1 text-white font-bold content-center text-center border-2 border-themeColor">
               MEFF
             </span>
             <span
               to={"/"}
-              className=" px-2 py-1.5 text-lg sm:text-xl md:text-2xl lg:text-3xl text-themeColor content-center hover:bg-themeColor hover:text-white border-themeColor border- border-2 text-center"
+              className="text-xl md:text-2xl lg:text-3xl md:py-2 sm:px-2 py-1 px-1 text-themeColor content-center hover:bg-themeColor hover:text-white border-themeColor border-2 text-center"
             >
               LIBRARY
             </span>
@@ -27,7 +27,7 @@ const SignUp = () => {
             </span>
             🧠
           </p>
-          <p className="text-lg md:text-xl mb-5 ">
+          <p className="text-lg md:text-xl mb-5">
             Sign up and unlock access to our rich collection of books,
             resources, and more.📚
           </p>
@@ -39,10 +39,10 @@ const SignUp = () => {
 
       <div className="flex justify-center items-center md:h-screen md:w-1/2">
         <form className="bg-white p-4 md:p-6 rounded-2xl shadow-xl max-w-md w-full">
-          <p className="text-2xl font-semibold text-[#0E345A] relative pl-10 md:mb-8">
+          <p className="text-2xl font-semibold text-themeColor relative pl-10 md:mb-8">
             Sign Up
-            <span className="mt-[4px] absolute left-[4px] top-[5px] w-4 h-4 bg-[#0E345A] rounded-full"></span>
-            <span className=" mt-[5px] absolute left-0 top-0 w-6 h-6 animate-pulse bg-[#0E345A] rounded-full opacity-10"></span>
+            <span className="mt-[4px] absolute left-[4px] top-[5px] w-4 h-4 bg-themeColor rounded-full"></span>
+            <span className=" mt-[5px] absolute left-0 top-0 w-6 h-6 animate-pulse bg-themeColor rounded-full opacity-10"></span>
           </p>
           <div className="flex gap-3 mb-1 mt-5">
             <label className="w-full relative">
@@ -89,7 +89,7 @@ const SignUp = () => {
 
           <button
             type="submit"
-            className="w-full bg-[#0E345A] text-white py-3 rounded-lg hover:bg-[#0e345ade] transition-colors mb-1 md:mb-5"
+            className="w-full bg-themeColor text-white py-3 rounded-lg hover:bg-[#0e345ade] transition-colors mb-1 md:mb-5"
           >
             <Link to="/login">Sign Up</Link>
           </button>
@@ -105,16 +105,16 @@ const SignUp = () => {
                 Google
               </button>
               <button className="flex gap-4 border-[0.1px] px-5 py-3 rounded-full w-full items-center justify-center hover:border-themeColor transition-colors ease-in-out">
-                <IonIcon name="logo-apple" className="text-2xl " />
+                <IonIcon name="logo-apple" className="text-2xl" />
                 Apple
               </button>
             </div>
           </div>
-          <p className="text-center text-base text-gray-600 mt-3 md:mt-5">
+          <p className="text-center text-base text-gray-600 mt-5">
             Already have an account?{" "}
             <Link
               className="text-blue-600 font-semibold hover:underline"
-              to="/login"
+              to={"/login"}
             >
               Sign in
             </Link>
